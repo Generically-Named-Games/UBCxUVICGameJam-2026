37c08603bf98396383f4d7e6f2695fb1df4ffc95
spencerishere <145073726+spencerishere@users.noreply.github.com>
2026-02-12 20:12:53 -0800

    added TweenManager class from old project - added manual test for tweens to main.lua - added README for all new folders - added event.lua as it's necessary for TweenManager - please see: https://github.com/spencrc/Unnamed-Pokemon-Fangame/blob/master/lib/tween-manager.lua

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,26 @@
+-- MODULES
 local window_functions = require("window_functions")
+local TM = require("/services/tween_manager").new()
+
+local dimensions = {
+	X = 0,
+	Y = 300,
+}
 
 function love.draw()
-	love.graphics.print("Hello World", 400, 300)
+	TM:update()
+	love.graphics.print("Hello World", dimensions.X, dimensions.Y)
 end
+
+local function playMyTween()
+	local myTween = TM:Create(dimensions, 500, "X", 500)
+
+	myTween.Completed:Connect(function()
+		dimensions.X = 0
+		playMyTween()
+	end)
+
+	myTween:Play()
+end
+
+playMyTween()
