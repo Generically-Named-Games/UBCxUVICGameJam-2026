903c0cbb00e8bd5aa17ccb6a9b13db1d9aded932
Spencer <145073726+spencrc@users.noreply.github.com>
2026-02-17 17:20:32 -0800

    Implement STI (#5)
    * added start files, updated readme, and created workflow
    
    added starter main.lua and .vscode files
    
    added assets folder
    
    added to github workflow; organized game files into src/ directory; updated readme
    
    * Fixed missing word in README
    
    * added TweenManager class from old project
    - added manual test for tweens to main.lua
    - added README for all new folders
    - added event.lua as it's necessary for TweenManager
    - please see: https://github.com/spencrc/Unnamed-Pokemon-Fangame/blob/master/lib/tween-manager.lua
    
    * fixed workflow for building
    
    fixed workflow not being in workflows folder
    
    Fixed build workflow to use nhartland/love-build
    
    Fix build workflow by adding id to love-build step
    
    * Added functions for window
    
    * Imported window functions
    
    * Added map
    
    * Adding functions for tile maps / layers
    
    * Image folder-Updated map with polyline (enemy path)
    
    * Image folder-Updated map with polyline (enemy path)
    
    * Delete src/assets/maps/Map01.lua
    
    * Delete src/assets/Images directory
    
    Dupliucate
    
    ---------
    Co-authored-by: Latisow <alexanderbarton01@gmail.com>

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,32 @@
+local sti = require("sti")
+local map_functions = require("map_functions")
+local window_functions = require("window_functions")
+
+-- Table of map objects
+local maps = {}
+
+--Load in the map and whether its layers are visible
+function love.load()
+	window_functions.setFullscreen("desktop")
+	maps.map1 = sti("assets/maps/map1.lua")
+	maps.map1.layers["Bushes"].visible = false
+end
+
+--Draws the map to fit the whole screen
+function love.draw()
+	if maps.map1 then
+		local mapWidth = maps.map1.width * maps.map1.tilewidth
+		local mapHeight = maps.map1.height * maps.map1.tileheight
+
+		local screenWidth = love.graphics.getWidth()
+		local screenHeight = love.graphics.getHeight()
+
+		local scaleX = screenWidth / mapWidth
+		local scaleY = screenHeight / mapHeight
+
+		maps.map1:draw(0, 0, scaleX, scaleY)
+	end
+end
 -- MODULES
 local window_functions = require("window_functions")
 local TM = require("/services/tween_manager").new()
